5c34c1fee25eef96116c14e7204b5f423c63671d
linchengdong <chengdong2518@163.com>
2022-07-14 18:45:13 +0800

    订单微服务跨域配置

--- tx-msg-order/src/main/java/com/dmai/transaction/order/config/CorsConfig.java
+++ tx-msg-order/src/main/java/com/dmai/transaction/order/config/CorsConfig.java
@@ -1,5 +1,11 @@
 package com.dmai.transaction.order.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 /**
  * <p>
  *
@@ -8,5 +14,20 @@ package com.dmai.transaction.order.config;
  * @author linchengdong
  * @since 2022-07-14 18:41:44
  */
+@Configuration
 public class CorsConfig {
+    private CorsConfiguration corsConfiguration() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/*",corsConfiguration());
+        return new CorsFilter(source);
+    }
 }
